eec5e68c315e0623d01607a35c3f75751741aab0
ehgud0670 <kimdo2297@gmail.com>
2020-09-22 20:28:30 +0900

    [#4] feat: close 버튼 추가 및 누르면 화면 내려가도록 기능 구현

--- GiphyApp/GiphyApp/Controllers/DetailViewController.swift
+++ GiphyApp/GiphyApp/Controllers/DetailViewController.swift
@@ -9,18 +9,40 @@
 import UIKit
 
 import Then
+import SnapKit
 
 final class DetailViewController: UIViewController {
+    private let closeButton = CloseButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureAttributes()
+        configureLayout()
     }
 }
 
 extension DetailViewController {
-    func configureAttributes() {
+    private func configureAttributes() {
         self.view.do {
             $0.backgroundColor = .blue
+        }
+        
+        closeButton.do {
+            $0.addTarget(self, action: #selector(close), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func close() {
+        self.dismiss(animated: true)
+    }
+    
+    private func configureLayout() {
+        self.view.addSubview(closeButton)
+        closeButton.snp.makeConstraints {
+            let constant = 27
+            $0.top.equalTo(self.view).offset(constant)
+            $0.trailing.equalTo(self.view).offset(-constant)
         }
     }
 }
