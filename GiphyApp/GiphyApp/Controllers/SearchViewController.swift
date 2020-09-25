94853ca89e6cfb72dea996ca7986471a0e1bb08c
ehgud0670 <kimdo2297@gmail.com>
2020-09-25 20:47:26 +0900

    [#13] feat: 검색 페이지에서 데이터 연동 불능인 경우 Alert을 띄우도록 함

--- GiphyApp/GiphyApp/Controllers/SearchViewController.swift
+++ GiphyApp/GiphyApp/Controllers/SearchViewController.swift
@@ -13,6 +13,7 @@ import SnapKit
 import RxSwift
 import RxCocoa
 import Kingfisher
+import Alamofire
 
 final class SearchViewController: UIViewController {
     // MARK: - UI
@@ -138,7 +139,9 @@ extension SearchViewController {
         gifsTask.perform(TrendRequest())
             .take(1)
             .do { [weak self] in self?.gifsTask.setIsLoadingFalse() }
-            .bind(onNext: { [weak self] in self?.gifsViewModel.updateFirst(with: $0)})
+            .subscribe(
+                onNext: { [weak self] in self?.gifsViewModel.updateFirst(with: $0) },
+                onError: { if $0.isSessionError { Util.presentAlertWithNetworkError(to: self) } })
             .disposed(by: disposeBag)
     }
     
@@ -148,7 +151,9 @@ extension SearchViewController {
         gifsTask.perform(SearchRequest(query: query))
             .take(1)
             .do { [weak self] in self?.gifsTask.setIsLoadingFalse() }
-            .bind(onNext: { [weak self] in self?.gifsViewModel.updateFirst(with: $0)})
+            .subscribe(
+                onNext: { [weak self] in self?.gifsViewModel.updateFirst(with: $0) },
+                onError: { if $0.isSessionError { Util.presentAlertWithNetworkError(to: self) } })
             .disposed(by: disposeBag)
     }
     
@@ -160,7 +165,9 @@ extension SearchViewController {
         gifsTask.perform(TrendRequest(offset: nextOffset))
             .take(1)
             .do { [weak self] in self?.gifsTask.setIsLoadingFalse() }
-            .bind(onNext: { [weak self] in self?.gifsViewModel.updateMore(with: $0)})
+            .subscribe(
+                onNext: { [weak self] in self?.gifsViewModel.updateMore(with: $0) },
+                onError: { if $0.isSessionError { Util.presentAlertWithNetworkError(to: self) } })
             .disposed(by: disposeBag)
     }
     
@@ -168,11 +175,12 @@ extension SearchViewController {
         guard !gifsTask.isLoading else { return }
         guard let pagination = gifsViewModel.pagination else { return }
         let nextOffset = pagination.count + pagination.offset
-        
         gifsTask.perform(SearchRequest(query: query, offset: nextOffset))
             .take(1)
             .do { [weak self] in self?.gifsTask.setIsLoadingFalse() }
-            .bind(onNext: { [weak self] in self?.gifsViewModel.updateMore(with: $0)})
+            .subscribe(
+                onNext: { [weak self] in self?.gifsViewModel.updateMore(with: $0) },
+                onError: { if $0.isSessionError { Util.presentAlertWithNetworkError(to: self) } })
             .disposed(by: disposeBag)
     }
 }
