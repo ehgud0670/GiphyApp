a6c40979ce5786f805ac63a26a663f923a19dccc
ehgud0670 <kimdo2297@gmail.com>
2020-09-25 22:39:25 +0900

    [#25] feat: Random Request 구현

--- GiphyApp/GiphyApp/Networks/Requests/RandomRequest.swift
+++ GiphyApp/GiphyApp/Networks/Requests/RandomRequest.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+protocol GifRequest: Request { }
+
+struct RandomRequest: GifRequest {
+    private let tag: String
+    
+    init(tag: String = "") {
+        self.tag = tag
+    }
+    
+    var path: String { "https://api.giphy.com/v1/gifs/random" }
+    var queryItems: [URLQueryItem]? {
+        [URLQueryItem(name: "api_key", value: "jzwguAwyjE6meldZ3VssS39SWnNA3kDF"),
+         URLQueryItem(name: "tag", value: tag)]
+    }
+}
