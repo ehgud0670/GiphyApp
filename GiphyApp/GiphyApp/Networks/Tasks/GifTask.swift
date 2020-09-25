e899f3ecafea5c7e8497f1829467418eda802892
ehgud0670 <kimdo2297@gmail.com>
2020-09-25 22:39:49 +0900

    [#25] feat: 단수의 데이터를 받는 GifTask 구현

--- GiphyApp/GiphyApp/Networks/Tasks/GifTask.swift
+++ GiphyApp/GiphyApp/Networks/Tasks/GifTask.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+import Alamofire
+import RxSwift
+
+final class GifTask {
+    typealias Input = GifRequest
+    typealias Output = GifResponse
+    
+    private let session: Session
+    private(set) var isLoading = false
+    
+    init(session: Session = AF) {
+        self.session = session
+    }
+    
+    func setIsLoadingFalse() {
+        isLoading = false
+    }
+    
+    func perform(_ request: Input) -> Observable<GifResponse> {
+        isLoading = true
+        return Observable.create { [weak self] emitter in
+            guard let self = self,
+            let urlRequest = request.urlRequest() else { return Disposables.create() }
+            
+            self.session.request(urlRequest)
+                .validate()
+                .responseDecodable(of: Output.self) { response in
+                    guard response.error == nil else {
+                        emitter.onError(response.error!)
+                        return
+                    }
+                    
+                    guard let giphyResponse = response.value else { return }
+                    emitter.onNext(giphyResponse)
+            }
+            return Disposables.create()
+        }
+    }
+}
